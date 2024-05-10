7df3ab19026b090af98ce7d2f371dec106835297
Rodrigo Silveira <r3850365@gmail.com>
2024-05-10 15:49:01 -0700

    Implement '/' handler

--- src/main.go
+++ src/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-  "github.com/labstack/echo/v4"
-)
-
-func main() {
-  e := echo.New()
-  e.Logger.Fatal(e.Start(":8080"))
-}
+	"github.com/labstack/echo/v4"
+	"madronetek.com/go-frameworks-eval/cmd/handlers"
+  )
+  
+  func main() {
+	e := echo.New()
+	e.GET("/", handlers.Home)
+	e.Logger.Fatal(e.Start(":8080"))
+  }
