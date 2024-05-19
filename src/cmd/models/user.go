c728353eb525b9cabef962c33dd56e34f43ad92d
Rodrigo Silveira <r3850365@gmail.com>
2024-05-19 06:36:52 -0700

    Found a mechanism to retrieve the request parameters

--- src/cmd/models/user.go
+++ src/cmd/models/user.go
@@ -1,6 +1,14 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/mail"
+	"time"
+
+	"gofr.dev/pkg/gofr"
+) 
 
 type User struct {
   Id        int64     `json:"id"`
@@ -9,4 +17,27 @@ type User struct {
   Password  string    `json:"password"`
   CreatedAt time.Time `json:"created_at"`
   UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (user *User) Create(ctx *gofr.Context) (interface{}, error) {
+	err := ctx.Bind(&user)
+	if err != nil {
+		return nil, errors.New("invalid param: body")
+	}
+
+	if !validEmail(user.Email) {
+		errorString := fmt.Sprintf("Invalid email: %s", user.Email);
+		return nil, errors.New(errorString)
+	}
+	name := ctx.Param("name")
+	slog.Info(fmt.Sprintf("Create, name = %s ", name))
+
+
+	return "user Create called", nil
+
+}
+
+func validEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+    return err == nil
 }
